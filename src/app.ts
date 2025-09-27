ce1d763eec341242d1d7bedd7088b2fd5d0b5d31
Mello <joaovdmello@gmail.com>
2025-09-27 18:32:56 -0300

    Caso de Uso e Design Patterns - Criação de um usuário

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
-import { faker } from '@faker-js/faker';
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { prisma } from './lib/prisma.js';
+
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post('/users', async (request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string().min(3),
+        email: z.string().email(),
+        password: z.string().min(6),
+    });
 
-prisma.user.create({
-    data: {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-    }
+    const { name, email, password } = registerBodySchema.parse(request.body);
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password, // Em um cenário real, nunca armazene senhas assim pqp
+        },
+    });
+
+    return reply.status(201).send();
 });
-
