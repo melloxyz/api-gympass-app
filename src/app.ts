bc4c76b9829b3c974ec048495c7ba38f94e5b73a
Mello <joaovdmello@gmail.com>
2025-09-27 16:39:48 -0300

    Integração com Prisma ORM - Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { faker } from '@faker-js/faker';
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+    }
+});
+
