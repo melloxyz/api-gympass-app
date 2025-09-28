8e4faa9c0c304b42d744e4367ea510aeda5fc127
Mello <joaovdmello@gmail.com>
2025-09-28 15:22:29 -0300

    Caso de Uso e Design Patterns - Hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import { z } from 'zod';
+import { hash } from 'bcryptjs';
 import { prisma } from '@/lib/prisma.js';
 
 export async function register(request: Fastify.FastifyRequest, reply: Fastify.FastifyReply) {
@@ -11,11 +12,25 @@ export async function register(request: Fastify.FastifyRequest, reply: Fastify.F
 
     const { name, email, password } = registerBodySchema.parse(request.body);
 
+    const password_hash = await hash(password, 6);
+
+    const findUserWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (findUserWithSameEmail) {
+        return reply.status(409).send({
+            message: 'Email already registered',
+        });
+    }
+
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password, // Em um cenário real, nunca armazene senhas assim pqp
+            password_hash,
         },
     });
 
