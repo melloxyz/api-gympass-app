95c9b2e884db1789a774c8ad738f9a2e341c6ad2
Mello <joaovdmello@gmail.com>
2025-09-29 08:56:51 -0300

    Design Patterns & Testes - Primeiro teste unitário

--- eslint.config.ts
+++ eslint.config.ts
@@ -8,7 +8,8 @@ export default defineConfig([
   tseslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     }
   }
 ]);
--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,1 +1,38 @@
-import { it, expect} from 'vitest'
+import { it, expect, describe } from 'vitest'
+import { RegisterUseCase } from './register.js'
+import { faker } from '@faker-js/faker'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+    it('should hash user password upon registration', async () => {
+        const registerUseCase = new RegisterUseCase({
+            async findByEmail(email: string) { 
+                return null 
+            },
+            async create(data: { name: string; email: string; password_hash: string }) {
+                return {
+                    id: faker.string.uuid(),
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    createdAt: new Date(),
+                }
+            },
+        })
+
+        const password = faker.internet.password()
+
+        const { user } = await registerUseCase.execute({
+            name: faker.person.firstName(),
+            email: faker.internet.email(),
+            password,
+        })
+        
+        const isPasswordCorrectlyHashed = await compare(
+            password,
+            user.password_hash
+        )
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    })
+}) 
