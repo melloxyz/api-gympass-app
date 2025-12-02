b46dfb90432d7ea3707f5d943722eeab1489fa21
Mello <joaovdmello@gmail.com>
2025-12-01 21:05:55 -0300

    Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,16 @@
+import { makeGetUserProfileUseCase } from '@/services/factories/make-get-user-profile-use-case.js';
 import Fastify from 'fastify';
 
 export async function profile(request: Fastify.FastifyRequest, reply: Fastify.FastifyReply) {
-    await request.jwtVerify();
 
-    return reply.status(200).send();
+    const GetUserProfil = makeGetUserProfileUseCase();
+
+    const { user } = await GetUserProfil.execute({
+        userId: request.user.sub,
+    });
+
+    return reply.status(200).send({ user: {
+        ...user,
+        password_hash: undefined, // REMOVE A SENHA DO RETORNO
+    } });
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,12 +2,13 @@ import fastify from 'fastify';
 import { register } from './controllers/register.js';
 import { authenticate } from './controllers/authenticate.js';
 import { profile } from './controllers/profile.js';
+import { verifyJWT } from './middlewares/verify-jwt.js';
 
 export async function appRoutes(app: fastify.FastifyInstance) {
     app.post('/users', register);
     app.post('/sessions', authenticate);
 
-    app.get('/me', profile);
+    app.get('/me', {onRequest: verifyJWT}, profile);
 }
 
 
